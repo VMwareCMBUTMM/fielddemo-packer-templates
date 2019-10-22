60bb0bd57b9bcfc6b7a34906d21fb1cffa21a1a5
sammcgeown <sam@mcgeown.co.uk>
2019-10-22 19:38:01 +0100

    Add timestamp to template creation

--- ubuntu-18.04/prepare-template.sh
+++ ubuntu-18.04/prepare-template.sh
@@ -10,8 +10,6 @@ if [ `id -u` -ne 0 ]; then
 	exit 1
 fi
 
-set -v
-
 # Clean Cloud-Init
 cloud-init clean
 
@@ -34,36 +32,6 @@ fi
 #cleanup /tmp directories
 rm -rf /tmp/*
 rm -rf /var/tmp/*
-
-#cleanup current ssh keys
-rm -f /etc/ssh/ssh_host_*
-
-#add check for ssh keys on reboot...regenerate if neccessary
-cat << 'EOL' | sudo tee /etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-# dynamically create hostname (optional)
-if hostname | grep localhost; then
-    hostnamectl set-hostname "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')"
-fi
-
-test -f /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
-exit 0
-EOL
-
-# make sure the script is executable
-chmod +x /etc/rc.local
 
 #reset hostname
 # prevent cloudconfig from preserving the original hostname
