4dd698070edc1c9c2cebd6f18de421b6880aa020
sammcgeown <sam@mcgeown.co.uk>
2020-05-11 14:22:13 +0100

    Update file provisioner

--- scripts/install_ssh_keys.sh
+++ scripts/install_ssh_keys.sh
@@ -1,6 +1,6 @@
 # Move the SSH key to Authorized Keys and ensure permissions
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-cat ~/id_autotmm.pub > ~/.ssh/authorized_keys
+cat /tmp/id_autotmm.pub > ~/.ssh/authorized_keys
 chmod 644 authorized_keys
 # rm ~/id_autotmm.pub
