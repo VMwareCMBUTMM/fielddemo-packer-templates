c6c0a2a6584200ca82c502fdcfae875d205012ae
sammcgeown <sam@mcgeown.co.uk>
2020-08-18 14:42:01 +0100

    Update public key name

--- scripts/ssh_install_autotmm_key.sh
+++ scripts/ssh_install_autotmm_key.sh
@@ -1,7 +1,7 @@
 # Move the SSH key to Authorized Keys and ensure permissions
 mkdir -p /home/autotmm/.ssh
 chmod 700 /home/autotmm/.ssh
-cat /tmp/id_autotmm.pub > /home/autotmm/.ssh/authorized_keys
+cat /tmp/id_rsa.pub > /home/autotmm/.ssh/authorized_keys
 chmod 644 /home/autotmm/.ssh/authorized_keys
 chown -R autotmm /home/autotmm/.ssh
-rm -rf /tmp/id_autotmm.pub
+rm -rf /tmp/id_rsa.pub
